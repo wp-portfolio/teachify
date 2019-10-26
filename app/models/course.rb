98e7be3b3083d1220bc6d257c900821e9ef50a2e
wp-portfolio <wpanowyk1@gmail.com>
2019-10-26 16:16:56 +0000

    student enrollment

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,10 @@
 class Course < ApplicationRecord
   belongs_to :user
   has_many :sections
+  has_many :enrollments
 
   mount_uploader :image, ImageUploader
-  
+
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
