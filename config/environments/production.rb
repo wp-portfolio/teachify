9a116bd12f24f27c8c2b8ac028c575f9db68ca41
wp-portfolio <wpanowyk1@gmail.com>
2019-10-26 09:20:29 +0000

    small typo with action mailer for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   #host pointing to app url
-  config.application_mailer.default_url_options = { host: 'teachify-will-panowyk.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'teachify-will-panowyk.herokuapp.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
