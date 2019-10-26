54607bb8e441b510ef5bcba5bfeb25b7e4aa57ee
wp-portfolio <wpanowyk1@gmail.com>
2019-10-26 17:49:33 +0000

    ordering lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,7 @@
 class Lesson < ApplicationRecord
   belongs_to :section
   mount_uploader :video, VideoUploader
+  #reorder items in db
+  include RankedModel
+  ranks :row_order, with_same: :section_id
 end
