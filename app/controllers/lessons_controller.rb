3f2e35f5a8ed4dfc1595083e6b2111398536f8a0
wp-portfolio <wpanowyk1@gmail.com>
2019-10-26 16:39:20 +0000

    enrollment permissions set up, space to add another tier of users

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_teacher_or_enrolled_user
+
   def show
   end
 
@@ -8,4 +11,10 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+  #can add another layer here if I want super users (such as paid vs free)
+  def require_teacher_or_enrolled_user
+    current_course = current_lesson.section.course
+    redirect_to course_path(current_course), alert: "Enroll to access the lessons!" unless current_user.enrolled_in?(current_course) || current_user.courses.include?(current_course)
+  end
+
 end
