26ff05fcef348c1672e40ed05a7b837ef676a6ac
wp-portfolio <wpanowyk1@gmail.com>
2019-10-27 02:00:32 +0000

    next lesson button

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,13 @@
 class Lesson < ApplicationRecord
   belongs_to :section
   mount_uploader :video, VideoUploader
+  
   #reorder items in db
   include RankedModel
   ranks :row_order, with_same: :section_id
+
+  def next_lesson
+    lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+    return lesson
+  end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -4,4 +4,13 @@ class Section < ApplicationRecord
 
   include RankedModel
   ranks :row_order, with_same: :course_id
+
+  
+  def next_section
+    section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
+    if lesson.blank? && section.next_section
+      return section.next_section.lessons.rank(:row_order).first
+    end
+    return section
+  end
 end
